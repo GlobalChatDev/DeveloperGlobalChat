7c3dbef7c4c3f27d6aa0a508ff39bc8fb289aada
JDJG Inc. Official <ghostzap123@gmail.com>
2021-11-21 01:54:21 +0000

    alright now I can log guild joins and leaves.

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -20,7 +20,52 @@ class Events(commands.Cog):
     print(f"Logged in as {self.bot.user}")
     print(f"Id: {self.bot.user.id}")
 
-  
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+    channels = [channel for channel in guild.channels]
+    roles = roles= [role for role in guild.roles]
+    embed = discord.Embed(title = f"Bot just joined : {guild.name}", color=random.randint(0,16777215))
+    
+    embed.set_thumbnail(url = guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
+
+    embed.add_field(name='Server Name:',value=f'{guild.name}')
+    embed.add_field(name='Server ID:',value=f'{guild.id}')
+    embed.add_field(name='Server region:',value=f'{guild.region}')
+    embed.add_field(name='Server Creation Date:', value=f"{discord.utils.format_dt(guild.created_at, style = 'd')}\n{discord.utils.format_dt(guild.created_at, style = 'T')}")
+    embed.add_field(name='Server Owner:',value=f'{guild.owner}')
+    embed.add_field(name='Server Owner ID:',value=f'{guild.owner_id}')
+    embed.add_field(name='Member Count:',value=f'{guild.member_count}')
+    embed.add_field(name = 'Amount of Channels:', value=f"{len(channels)}")
+    embed.add_field(name = 'Amount of Roles:',value=f"{len(roles)}")
+    await self.bot.get_channel(852897595869233182).send(embed=embed)
+
+  @commands.Cog.listener()
+  async def on_guild_remove(self, guild):
+    channels = [channel for channel in guild.channels]
+    roles = roles= [role for role in guild.roles]
+    embed = discord.Embed(title = f"Bot just left : {guild.name}", color = random.randint(0,16777215))
+    
+    embed.set_thumbnail(url = guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
+
+    embed.add_field(name = 'Server Name:', value = f'{guild.name}')
+    embed.add_field(name = 'Server ID:', value = f'{guild.id}')
+
+    try:
+      embed.add_field(name = 'Server region:', value=f'{guild.region}')
+
+    except:
+      pass
+
+    embed.add_field(name='Server Creation Date:', value=f"{discord.utils.format_dt(guild.created_at, style = 'd')}\n{discord.utils.format_dt(guild.created_at, style = 'T')}")
+    embed.add_field(name='Server Owner:',value=f'{guild.owner}')
+    embed.add_field(name='Server Owner ID:',value=f'{guild.owner_id}')
+    try:
+      embed.add_field(name='Member Count:',value=f'{guild.member_count}')
+    except:
+      pass
+    embed.add_field(name='Amount of Channels:',value=f"{len(channels)}")
+    embed.add_field(name = 'Amount of Roles:',value=f"{len(roles)}")
+    await self.bot.get_channel(852897595869233182).send(embed=embed)
 
 def setup(bot):
   bot.add_cog(Events(bot))
