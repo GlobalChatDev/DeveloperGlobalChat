96513e12a84710eba15b41b63c5ed0632f7b0145
BenitzCoding <benitzcoding@yahoo.com>
2021-11-22 09:18:28 +0800

    Removed unneccesary code for mongo migration on main file.

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ from discord.ext import commands
 import asyncio, os, traceback
 import B
 
-class GlobalChatBot(commands.Bot):
+class GlobalChatBot(commands.AutoShardedBot):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
 
   async def start(self, *args, **kwargs):
-    self.db = await asyncpg.create_pool(os.getenv("DB_key"))
-
-    self.linked_data = await self.db.fetch("SELECT * FROM linked_chat")
-    self.linked_channels = [c.get("channel_id") for c in self.linked_data]
-
-    #grab from guild_bans - guild bans
-    #bans - user bans (blacklist)
-
+    self.db = MongoClient(os.getenv("DB_url"))['GlobalChat']
     await super().start(*args, **kwargs)
 
   async def close(self):
-    await self.db.close()
     await super().close()
 
 bot = GlobalChatBot(command_prefix = commands.when_mentioned_or("d!"), intents = discord.Intents.all(), owner_ids = [168422909482762240, 529499034495483926, 745058406083198994], activity = discord.Activity(type = discord.ActivityType.listening, name = f"I am making developer help across guilds possible."))
