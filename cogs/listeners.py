9f3854642963afa325b0608c8c9c23db8f07d99f
JDJG Inc. Official <ghostzap123@gmail.com>
2022-02-27 18:33:49 -0500

    Revert "Improve code readability and cleanup (#3)" (#4)
    This reverts commit 4a5eadaec93cb48f93eade7ba84718594ccef76c.

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,33 +1,28 @@
-from typing import TYPE_CHECKING
-import random
-
-import discord
 from discord.ext import commands
-
-if TYPE_CHECKING:
-    from main import GlobalChatBot
-else:
-    GlobalChatBot = commands.Bot
+import discord, random
 
 
 class Events(commands.Cog):
-    def __init__(self, bot: GlobalChatBot):
-        self.bot: GlobalChatBot = bot
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Bot is Ready", f"Logged in as {self.bot.user}", f"ID: {self.bot.user.id}", sep="\n")
+        print("Bot is Ready")
+        print(f"Logged in as {self.bot.user}")
+        print(f"Id: {self.bot.user.id}")
 
     @commands.Cog.listener()
-    async def on_guild_join(self, guild: discord.Guild):
+    async def on_guild_join(self, guild):
         channels = [channel for channel in guild.channels]
         roles = roles = [role for role in guild.roles]
         embed = discord.Embed(title=f"Bot just joined  {guild.name}", color=random.randint(0, 16777215))
 
-        embed.set_thumbnail(url=str(guild.icon or "https://i.imgur.com/3ZUrjUP.png"))
+        embed.set_thumbnail(url=guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
 
         embed.add_field(name="Server Name:", value=f"{guild.name}")
         embed.add_field(name="Server ID:", value=f"{guild.id}")
+        embed.add_field(name="Server region:", value=f"{guild.region}")
         embed.add_field(
             name="Server Creation Date:",
             value=f"{discord.utils.format_dt(guild.created_at, style = 'd')}\n{discord.utils.format_dt(guild.created_at, style = 'T')}",
@@ -40,15 +35,21 @@ class Events(commands.Cog):
         await self.bot.get_channel(852897595869233182).send(embed=embed)
 
     @commands.Cog.listener()
-    async def on_guild_remove(self, guild: discord.Guild):
+    async def on_guild_remove(self, guild):
         channels = [channel for channel in guild.channels]
         roles = roles = [role for role in guild.roles]
         embed = discord.Embed(title=f"Bot just left : {guild.name}", color=random.randint(0, 16777215))
 
-        embed.set_thumbnail(url=str(guild.icon or "https://i.imgur.com/3ZUrjUP.png"))
+        embed.set_thumbnail(url=guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
 
         embed.add_field(name="Server Name:", value=f"{guild.name}")
         embed.add_field(name="Server ID:", value=f"{guild.id}")
+
+        try:
+            embed.add_field(name="Server region:", value=f"{guild.region}")
+
+        except:
+            pass
 
         embed.add_field(
             name="Server Creation Date:",
@@ -58,7 +59,7 @@ class Events(commands.Cog):
         embed.add_field(name="Server Owner ID:", value=f"{guild.owner_id}")
         try:
             embed.add_field(name="Member Count:", value=f"{guild.member_count}")
-        except AttributeError:
+        except:
             pass
         embed.add_field(name="Amount of Channels:", value=f"{len(channels)}")
         embed.add_field(name="Amount of Roles:", value=f"{len(roles)}")
--- utils/buttons.py
+++ utils/buttons.py
@@ -1,27 +1,30 @@
-from typing import Optional
-
 import discord
 
 
 class BasicButtons(discord.ui.View):
-    def __init__(self, ctx, *, timeout: float = 180.0):
-        super().__init__(timeout=timeout)
+    def __init__(self, ctx, **kwargs):
+        super().__init__(**kwargs)
         self.ctx = ctx
-        self.value: Optional[bool] = None
+        self.value: str = None
 
     @discord.ui.button(label="Accept", style=discord.ButtonStyle.success, emoji="✅")
     async def accept(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.edit_message(view=None)
+
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
         self.value = True
         self.stop()
 
     @discord.ui.button(label="Deny", style=discord.ButtonStyle.danger, emoji="❌")
     async def denied(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.edit_message(view=None)
+
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
         self.value = False
         self.stop()
 
     async def interaction_check(self, item: discord.ui.Item, interaction: discord.Interaction):
+
         if self.ctx.author.id != interaction.user.id:
             return await interaction.response.send_message(
                 content=f"You Can't Use that button, {self.ctx.author.mention} is the author of this message.",
